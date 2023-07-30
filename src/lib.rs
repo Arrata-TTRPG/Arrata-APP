8288acf85c2eb23a336a83b2eb8f03fd83faeb8a
Kaleb Burris <kaleb.r.burris@gmail.com>
2023-07-30 00:57:54 -0800

    Basic write functionality; started building character struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,56 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+};
+
 use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Character {
+    pub name: String,
+    pub stock: String,
+}
+
+impl Character {
+    pub fn new() -> Character {
+        Character {
+            name: "John Arrata".to_string(),
+            stock: "Human".to_string(),
+        }
+    }
+}
+
+fn write_to_file(character: Character) {
+    let f = File::create("char.arrata").unwrap();
+
+    let mut writer = BufWriter::new(f);
+
+    let character_serde = serde_json::to_string(&character).unwrap();
+
+    writer.write(character_serde.as_bytes()).unwrap();
+}
 
 pub fn app(cx: Scope) -> Element {
-    cx.render(rsx!{
-        div {
-            "Hello, Arrata!"
+
+    let character = use_state(cx, || Character::new());
+
+    cx.render(rsx! {
+        input {
+            value: "{character.name}",
+            oninput: move |evt| {
+                character.make_mut().name = evt.value.clone();
+            },
+        }
+        input {
+            value: "{character.stock}",
+            oninput: move |evt| {
+                character.make_mut().stock = evt.value.clone();
+            },
+        }
+        button {
+            onclick: move |_event| write_to_file(character.get().clone()),
+            "Save Character"
         }
     })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![windows_subsystem = "windows"]
 
 use arrata_app::*;
+use dioxus_desktop::{Config, WindowBuilder};
 
 fn main() {
-    dioxus_desktop::launch(app);
+    dioxus_desktop::launch_cfg(
+        app,
+        Config::default().with_window(
+            WindowBuilder::new().with_title("Arrata Character Sheet Manager")
+        )
+    );
 }
