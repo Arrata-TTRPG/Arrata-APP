a3ab24ed83af7a498b011aa597babcc87dce9a1d
Kaleb Burris <kaleb.r.burris@gmail.com>
2024-01-20 23:05:26 -0900

    chore: Add cfg_attr to allow debug outputs on windows

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#![windows_subsystem = "windows"]
+// If we're not in a prod compilation, allow a window for debug prints
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use arrata_app::*;
 use dioxus_desktop::{Config, WindowBuilder};
@@ -9,6 +10,6 @@ fn main() {
         Config::new()
             .with_window(WindowBuilder::new().with_title("Arrata Character Sheet Manager"))
             //.with_icon(icon)
-            .with_custom_head(r#"<link rel="stylesheet" href="public/tailwind.css">"#.to_string()),
+            .with_custom_head(r#"<link rel="stylesheet" href="public/tailwind.css">"#.into()),
     );
 }
