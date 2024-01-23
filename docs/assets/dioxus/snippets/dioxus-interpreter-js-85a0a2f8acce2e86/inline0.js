0ca7ae0837c6173ab93a9994f0c2b92fb8d13f7e
Kaleb Burris <kaleb.r.burris@gmail.com>
2024-01-23 11:02:45 -0900

    chore: Add docs to dev

--- docs/assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
+++ docs/assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
@@ -169,7 +169,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    let u32buf,u32bufp;const evt = [];
+    const evt = [];
                     let evt_tmp1, evt_tmp2;
                     function get_evt() {
                         evt_tmp2 = u8buf[u8bufp++];
@@ -181,7 +181,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return evt[evt_tmp2&4294967167];
                         }
-                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();const attr = [];
+                    }const attr = [];
                     let attr_tmp1, attr_tmp2;
                     function get_attr() {
                         attr_tmp2 = u8buf[u8bufp++];
@@ -193,7 +193,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return attr[attr_tmp2&4294967167];
                         }
-                    }let u8buf,u8bufp;const ns_cache = [];
+                    }let u32buf,u32bufp;const ns_cache = [];
                     let ns_cache_tmp1, ns_cache_tmp2;
                     function get_ns_cache() {
                         ns_cache_tmp2 = u8buf[u8bufp++];
@@ -205,8 +205,8 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
-                    }
-            let ns,event_name,value,field,bubbles,ptr,len,id;
+                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();let u8buf,u8bufp;
+            let ns,value,field,bubbles,event_name,ptr,len,id;
             export function create(r){
                 d=r;
             }
