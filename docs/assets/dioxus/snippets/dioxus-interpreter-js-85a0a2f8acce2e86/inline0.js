b8aa76144d9c69209368c5895db7a70343e38276
kalebvonburris <31938062+kalebvonburris@users.noreply.github.com>
2024-01-26 21:48:35 +0000

    chore(cd): Deploy to GH Pages for vv0.0.18

--- docs/assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
+++ docs/assets/dioxus/snippets/dioxus-interpreter-js-85a0a2f8acce2e86/inline0.js
@@ -169,19 +169,7 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
       function truthy(val) {
         return val === "true" || val === true;
       }
-    let u32buf,u32bufp;let s = "";let lsp,sp,sl; let c = new TextDecoder();const evt = [];
-                    let evt_tmp1, evt_tmp2;
-                    function get_evt() {
-                        evt_tmp2 = u8buf[u8bufp++];
-                        if(evt_tmp2 & 128){
-                            evt_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
-                            evt[evt_tmp2&4294967167]=evt_tmp1;
-                            return evt_tmp1;
-                        }
-                        else{
-                            return evt[evt_tmp2&4294967167];
-                        }
-                    }const attr = [];
+    let u32buf,u32bufp;const attr = [];
                     let attr_tmp1, attr_tmp2;
                     function get_attr() {
                         attr_tmp2 = u8buf[u8bufp++];
@@ -205,8 +193,20 @@ let m,p,ls,d,t,op,i,e,z,metaflags;
                         else{
                             return ns_cache[ns_cache_tmp2&4294967167];
                         }
-                    }let u8buf,u8bufp;
-            let field,ptr,id,ns,value,event_name,len,bubbles;
+                    }let s = "";let lsp,sp,sl; let c = new TextDecoder();let u8buf,u8bufp;const evt = [];
+                    let evt_tmp1, evt_tmp2;
+                    function get_evt() {
+                        evt_tmp2 = u8buf[u8bufp++];
+                        if(evt_tmp2 & 128){
+                            evt_tmp1=s.substring(sp,sp+=u8buf[u8bufp++]);
+                            evt[evt_tmp2&4294967167]=evt_tmp1;
+                            return evt_tmp1;
+                        }
+                        else{
+                            return evt[evt_tmp2&4294967167];
+                        }
+                    }
+            let field,ns,event_name,bubbles,id,len,ptr,value;
             export function create(r){
                 d=r;
             }
