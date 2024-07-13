ed8b54b451927ebaaf0fe0df2b87cf614a78ebb9
Kaleb Burris <kaleb.r.burris@gmail.com>
2024-07-13 01:17:14 -0800

    feat: Add reset character button

--- src/render/character.rs
+++ src/render/character.rs
@@ -10,21 +10,21 @@ use crate::{
 #[component]
 pub(crate) fn RenderCharacter() -> Element {
     rsx! {
-        div { class: "flex flex-wrap content-center items-center justify-center",
-            div { class: "flex",
+        div { class: "flex flex-wrap sm:flex-row flex-col content-center items-center justify-center gap-2 px-2",
+            div { class: "flex sm:w-fit w-full",
                 h2 { class: "font-mono text-xl p-2", "Name:" }
                 input {
-                    class: "border-spacing-1 border rounded-lg p-2",
+                    class: "border-spacing-1 border rounded-lg p-2 text-center sm:flex-grow-0 flex-grow",
                     value: "{CHARACTER().name}",
                     oninput: move |evt| {
                         CHARACTER.write().name = evt.value();
                     }
                 }
             }
-            div { class: "flex",
+            div { class: "flex sm:w-fit w-full",
                 h2 { class: "font-mono text-xl p-2", "Stock:" }
                 input {
-                    class: "border-spacing-1 border rounded-lg p-2",
+                    class: "border-spacing-1 border rounded-lg p-2 text-center sm:flex-grow-0 flex-grow",
                     value: "{CHARACTER().stock}",
                     oninput: move |evt| {
                         CHARACTER.write().stock = evt.value();
@@ -44,7 +44,7 @@ pub(crate) fn RenderCharacter() -> Element {
 #[component]
 pub(crate) fn CharacterIO() -> Element {
     rsx! {
-        div { class: "px-5 py-5 font-mono origin-center justify-center text-center self-center items-center content-center flex space-x-3",
+        div { class: "px-5 py-5 font-mono origin-center justify-center text-center self-center items-center content-center flex flex-wrap gap-2",
             button {
                 class: "font-mono text-xl bg-slate-900 hover:bg-slate-600 text-white font-bold py-2 px-4 rounded",
                 onclick: move |_| {
@@ -78,6 +78,11 @@ pub(crate) fn CharacterIO() -> Element {
                     });
                 },
                 "Load Character"
+            }
+            button {
+                class: "font-mono text-xl bg-red-900 hover:bg-red-950 text-white font-bold py-2 px-4 rounded",
+                onclick: move |_| *CHARACTER.write() = Character::default(),
+                "Reset Character"
             }
         }
     }
