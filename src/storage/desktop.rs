b431c35e2a6bd51d4e9778e2710fa8e65757c242
Kaleb Burris <kaleb.r.burris@gmail.com>
2024-07-12 23:19:05 -0800

    chore: Add new files

---
+++ src/storage/desktop.rs
@@ -0,0 +1,45 @@
+use std::sync::OnceLock;
+
+use arrata_lib::Character;
+
+use crate::{CHARACTER, VERSION};
+
+pub static LOCATION: OnceLock<std::path::PathBuf> = OnceLock::new();
+
+/// Sets the directory path.
+///
+/// # Panics
+///
+/// This function will panic if the provided path is invalid.
+pub fn set_directory(path: std::path::PathBuf) {
+    LOCATION.set(path).unwrap();
+}
+
+/// Writes the character data to a file.
+///
+/// # Panics
+///
+/// This function will panic if it fails to write the character data to the file.
+pub fn write_character(name: &str) {
+    if let Some(path) = LOCATION.get() {
+        let character_file = format!("{name}-{VERSION}.arrata");
+        let file_path = path.join(character_file);
+        if let Ok(file) = std::fs::write(file_path, serde_json::to_string(&CHARACTER()).unwrap()) {
+            println!("Character saved: {file:?}");
+        }
+    }
+}
+
+pub fn read_character(name: &str) -> Option<Character> {
+    if let Some(path) = LOCATION.get() {
+        let character_file = format!("{name}-{VERSION}.arrata");
+        let file_path = path.join(character_file);
+        if let Ok(file) = std::fs::read(file_path) {
+            if let Ok(character) = serde_json::from_slice(&file) {
+                return Some(character);
+            }
+        }
+    }
+
+    None
+}
---
+++ src/storage/web.rs
@@ -0,0 +1,63 @@
+use arrata_lib::Character;
+use base64::prelude::*;
+use dioxus::prelude::eval;
+
+use crate::{CHARACTER, VERSION};
+
+/// Writes the character data to a file.
+///
+/// # Panics
+///
+/// This function will panic if it fails to write the character data to the file.
+pub async fn write_character(name: &str) {
+    println!("Writing character: {name}");
+
+    let character = CHARACTER();
+    let encoded = BASE64_STANDARD.encode(serde_json::to_string(&character).unwrap());
+
+    let eval = eval(
+        "
+        const fileName = await dioxus.recv();
+        const fileData = await dioxus.recv();
+        localStorage.setItem(fileName, fileData);
+    ",
+    );
+
+    eval.send(serde_json::Value::String(format!("{}-{}", name, VERSION())))
+        .unwrap();
+    eval.send(serde_json::Value::String(encoded)).unwrap();
+
+    eval.await.unwrap();
+}
+
+/// Reads the character data from a file.
+///
+/// # Panics
+///
+/// This function will panic if it fails to read the character data from the file.
+pub async fn read_character(name: &str) -> Option<Character> {
+    let mut eval = eval(
+        "
+        const fileName = await dioxus.recv();
+        const fileData = localStorage.getItem(fileName);
+        dioxus.send(fileData);
+    ",
+    );
+
+    eval.send(serde_json::Value::String(format!("{}-{}", name, VERSION())))
+        .unwrap();
+
+    let data = eval.recv().await.unwrap();
+
+    log::info!("Got value: {data:?}");
+
+    if let Some(data) = data.as_str() {
+        if let Ok(decoded) = BASE64_STANDARD.decode(data.as_bytes()) {
+            if let Ok(character) = serde_json::from_slice(&decoded) {
+                return Some(character);
+            }
+        }
+    }
+
+    None
+}
