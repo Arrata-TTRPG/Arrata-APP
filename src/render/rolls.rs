b72dfe8e787e57ceccceaecdabf1c49c6d34b079
Kaleb Burris <kaleb.r.burris@gmail.com>
2024-08-20 13:22:22 -0800

    fix: Actually scroll overflow dice results

--- src/render/rolls.rs
+++ src/render/rolls.rs
@@ -15,9 +15,9 @@ pub(crate) fn RenderRolls() -> Element {
     let mut advantage = use_signal(|| 0);
     let mut disadvantage = use_signal(|| 0);
     rsx! {
-        div { class: "z-10 fixed flex justify-center content-center max-w-[80%] max-h-[80%] h-fit w-96 border text-white border-white bg-slate-800 m-auto left-0 right-0 top-0 bottom-0 rounded-lg",
+        div { class: "z-10 fixed flex flex-col items-center max-w-[80%] max-h-[80%] h-fit w-96 overflow-hidden border text-white border-white bg-slate-800 m-auto left-0 right-0 top-0 bottom-0 rounded-lg gap-2 pt-2",
             // Close button
-            div { class: "z-20 absolute right-0 p-2",
+            div { class: "z-20 absolute right-0 top-0 p-2",
                 div {
                     class: "bg-slate-950 hover:bg-slate-700 rounded cursor-pointer",
                     onclick: move |_| {
@@ -30,89 +30,76 @@ pub(crate) fn RenderRolls() -> Element {
                     Icon { width: 35, height: 35, fill: "red", icon: BsX }
                 }
             }
-            div { class: "flex flex-col max-h-full content-center justify-items-center",
-                // Stat
-                div { class: "content-center",
-                    // Stat Name
-                    div { class: "flex justify-center p-2",
-                        h2 { class: "w-fit text-2xl text-center font-mono p-2 bg-slate-900 rounded border",
-                            "{stat.name}"
-                        }
-                    }
-                    // Quality + Quantity
-                    div { class: "inline-flex w-full justify-center justify-items-center content-center",
-                        div { class: "text-xl justify-center font-mono p-2", "{stat.quality}" }
-                        div { class: "text-xl justify-center font-mono p-2", "{stat.quantity}" }
-                        // Rolling
-                        div { class: "justify-center",
-                            button {
-                                class: "max-w-fit font-mono place-self-center bg-slate-900 hover:bg-slate-600 p-2 rounded",
-                                onclick: move |_| {
-                                    dice_results
-                                        .with_mut(|results| {
-                                            *results = Some(roll_stat(&stat, advantage(), disadvantage()));
-                                        });
-                                },
-                                "Roll!"
-                            }
-                        }
-                    }
-                    // Advantage + Disadvantage
-                    div { class: "grid grid-cols-2 justify-center content-even",
-                        div { class: "p-2",
-                            div { class: "p-1 items-center justify-center bg-green-950 rounded border",
-                                div { class: "font-mono text-center", "Advantage" }
-                                div { class: "flex justify-center",
-                                    input {
-                                        class: "w-12 border rounded-lg p-1 appearance-none",
-                                        r#type: "number",
-                                        value: "{advantage()}",
-                                        min: 0,
-                                        max: i64::MAX,
-                                        oninput: move |evt| advantage.set(evt.value().parse::<usize>().unwrap_or(0))
-                                    }
-                                }
-                            }
-                        }
-                        div { class: "p-2",
-                            div { class: "p-1 items-center justify-center bg-red-950 rounded border",
-                                div { class: "font-mono text-center", "Disadvantage" }
-                                div { class: "flex justify-center",
-                                    input {
-                                        class: "w-12 border rounded-lg p-1",
-                                        r#type: "number",
-                                        value: "{disadvantage()}",
-                                        min: 0,
-                                        max: i64::MAX,
-                                        oninput: move |evt| disadvantage.set(evt.value().parse::<usize>().unwrap_or(0))
-                                    }
-                                }
-                            }
+            // Stat Name
+            h2 { class: "text-2xl text-center font-mono p-2 bg-slate-900 rounded border",
+                "{stat.name}"
+            }
+            // Quality + Quantity
+            div { class: "inline-flex w-full justify-center place-items-center content-center gap-2",
+                p { class: "text-xl font-mono", "{stat.quality}" }
+                p { class: "text-xl font-mono", "{stat.quantity}" }
+                // Rolling
+                button {
+                    class: "max-w-fit font-mono place-self-center bg-slate-900 hover:bg-slate-600 p-2 rounded",
+                    onclick: move |_| {
+                        dice_results
+                            .with_mut(|results| {
+                                *results = Some(roll_stat(&stat, advantage(), disadvantage()));
+                            });
+                    },
+                    "Roll!"
+                }
+            }
+            // Advantage + Disadvantage
+            div { class: "grid grid-cols-2 justify-center content-even gap-4",
+                div { class: "p-1 items-center justify-center bg-green-950 rounded border",
+                    div { class: "font-mono text-center", "Advantage" }
+                    div { class: "flex justify-center",
+                        input {
+                            class: "w-12 border rounded-lg p-1 appearance-none",
+                            r#type: "number",
+                            value: "{advantage()}",
+                            min: 0,
+                            max: i64::MAX,
+                            oninput: move |evt| advantage.set(evt.value().parse::<usize>().unwrap_or(0))
                         }
                     }
                 }
-
-                if let Some(results) = dice_results() {
-                    div { class: "flex flex-col font-mono justify-center max-h-full overflow-y-hidden",
-                        div { class: "flex justify-center",
-                            // Successes
-                            div { class: "text-center text-green-600 p-2",
-                                "Successes: {results.successes}"
-                            }
-                            // Failures
-                            div { class: "text-center text-red-600 p-2", "Failures: {results.failures}" }
+                div { class: "p-1 items-center justify-center bg-red-950 rounded border",
+                    div { class: "font-mono text-center", "Disadvantage" }
+                    div { class: "flex justify-center",
+                        input {
+                            class: "w-12 border rounded-lg p-1",
+                            r#type: "number",
+                            value: "{disadvantage()}",
+                            min: 0,
+                            max: i64::MAX,
+                            oninput: move |evt| disadvantage.set(evt.value().parse::<usize>().unwrap_or(0))
                         }
-                        div { class: "p-2 text-lg text-center", "Results" }
-                        // Results
-                        div { class: "p-1 overflow-y-scroll max-h-full flex flex-wrap w-full content-around gap-1 justify-center items-start text-center border rounded bg-slate-900",
-                            for r in results.results.iter() {
-                                div {
-                                    class: format!(
-                                        "px-1 bg-slate-950 rounded {}",
-                                        if *r >= stat.quality as u8 { "text-green-500" } else { "text-red-600" },
-                                    ),
-                                    "{r}"
-                                }
+                    }
+                }
+            }
+
+            if let Some(results) = dice_results() {
+                div { class: "flex flex-col font-mono justify-center max-h-full overflow-y-hidden pt-2",
+                    div { class: "flex justify-center",
+                        // Successes
+                        div { class: "text-center text-green-600 p-2",
+                            "Successes: {results.successes}"
+                        }
+                        // Failures
+                        div { class: "text-center text-red-600 p-2", "Failures: {results.failures}" }
+                    }
+                    h2 { class: "p-2 text-lg text-center", "Results" }
+                    // Results
+                    div { class: "p-1 overflow-scroll flex flex-wrap justify-center max-h-full w-full gap-1 border rounded bg-slate-900",
+                        for r in results.results.iter() {
+                            div {
+                                class: format!(
+                                    "px-1 bg-slate-950 rounded {}",
+                                    if *r >= stat.quality as u8 { "text-green-500" } else { "text-red-600" },
+                                ),
+                                "{r}"
                             }
                         }
                     }
